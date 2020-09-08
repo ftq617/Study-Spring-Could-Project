cecce4507dd13cc78edea81eb380193227c411dc
TENCENT\v_taiquanfu <1506958820@>
2020-09-08 16:15:03 +0800

    开启

--- hystrix-ribbon-service/src/main/java/com/luke/study/ribbon/controller/RibbonController.java
+++ hystrix-ribbon-service/src/main/java/com/luke/study/ribbon/controller/RibbonController.java
@@ -28,6 +28,14 @@ public class RibbonController {
 
     private static Integer total=0;
 
+    //@HystrixCommand中的常用参数
+    //
+    //fallbackMethod：指定服务降级处理方法；
+    //ignoreExceptions：忽略某些异常，不发生服务降级；
+    //commandKey：命令名称，用于区分不同的命令；
+    //groupKey：分组名称，Hystrix会根据不同的分组来统计命令的告警及仪表盘信息；
+    //threadPoolKey：线程池名称，用于划分线程池。
+    //
     @GetMapping("test")
     @HystrixCommand(fallbackMethod = "getDefaultRibbonData",
             commandKey = "getUserCommand",
